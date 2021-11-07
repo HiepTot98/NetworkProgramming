232f4273c9fafb14c32b90114de131cd18015c43
HiepTot98 <Hsee131998@gmail.com>
2021-11-08 03:11:28 +0700

    material v.10

---
+++ abc/js/takeQuiz.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Represents the current question
+currentID = 1;
+//next question
+function nextQ(number) {
+
+//  Hide current question
+    document.getElementById("q" + currentID).classList.add("hidden");
+
+    currentID = (currentID + 1) % number;
+
+//  Case last question
+    if (currentID == 0) {
+
+//       show button finish
+        document.getElementById("finish").classList.remove("hidden");
+        currentID = number;
+    }
+//       hide button finish when the user returns to the first question  
+    if (currentID == 1) {
+        document.getElementById("finish").classList.add("hidden");
+    }
+//    show next question
+    document.getElementById("q" + currentID).classList.remove("hidden");
+}
+
